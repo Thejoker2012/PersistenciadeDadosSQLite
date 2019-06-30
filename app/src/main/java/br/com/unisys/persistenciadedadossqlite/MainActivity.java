8168c4b0913b8e5962a5bdcc0bad069966b75ff5
Iago <iagofalcom@hotmail.com>
2019-06-30 20:48:52 -0300

    Criando Banco de Dados
    *try{}catch(){}
    *SQLiteDatabase bancoDados = openOrCreateDatabase("cadastro",MODE_PRIVATE,null);
    *bancoDados.execSQL("INSERT INTO pessoas(nome,idade) VALUES('Iago',26)");
    *Cursor cursor = bancoDados.rawQuery("SELECT nome,idade FROM pessoas",null);
    *int indiceNome = cursor.getColumnIndex("nome");
    *cursor.moveToFirst();
    *while (cursor != null)
    *Log.i("RESULTADO - nome: ",cursor.getString(indiceNome));
    *cursor.moveToNext();

--- app/src/main/java/br/com/unisys/persistenciadedadossqlite/MainActivity.java
+++ app/src/main/java/br/com/unisys/persistenciadedadossqlite/MainActivity.java
@@ -1,6 +1,8 @@
 package br.com.unisys.persistenciadedadossqlite;
 
 import android.content.SharedPreferences;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -9,6 +11,7 @@ import com.google.android.material.snackbar.Snackbar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -26,6 +29,35 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        try{
+            //Criando o banco de dados
+            SQLiteDatabase bancoDados = openOrCreateDatabase("cadastro",MODE_PRIVATE,null);
+
+            //Criando tabela e colunas
+            bancoDados.execSQL("CREATE TABLE IF NOT EXISTS pessoas(nome VARCHAR, idade INT(3))");
+
+            //Inserindo dados
+           /* bancoDados.execSQL("INSERT INTO pessoas(nome,idade) VALUES('Iago',26)");
+            bancoDados.execSQL("INSERT INTO pessoas(nome,idade) VALUES('Igor',20)");*/
+
+            //Recuperar pessoas
+            Cursor cursor = bancoDados.rawQuery("SELECT nome,idade FROM pessoas",null);
+
+            int indiceNome = cursor.getColumnIndex("nome");
+            int indiceIdade = cursor.getColumnIndex("idade");
+
+            cursor.moveToFirst();
+            while (cursor != null){
+                Log.i("RESULTADO - nome: ",cursor.getString(indiceNome));
+                Log.i("RESULTADO - idade: ",cursor.getString(indiceIdade));
+                cursor.moveToNext();
+            }
+
+
+        }catch(Exception e){
+            e.printStackTrace();
+        }
 
         editNome = findViewById(R.id.editNome);
         listView = findViewById(R.id.listView);
